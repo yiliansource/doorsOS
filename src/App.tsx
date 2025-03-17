8b40446c50b762fa1600ef7f62ebd9e4ed649931
mellbacon <mellodev@outlook.com>
2025-03-16 19:05:16 -0500

    added desktop component
    note: removed h-screen because it was conflicting with desktop interactivity
    Signed-off-by: mellbacon <mellodev@outlook.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
+import { Desktop } from "./components/Desktop";
 import { Taskbar } from "./components/taskbar";
 
 function App() {
     return (
-        <main className="relative w-screen h-screen">
-            <div className="fixed -z-10 top-0 left-0 w-full h-full">
-                <img src="/background.jpg" className="w-full h-full" />
-            </div>
-
+        <main className="relative w-screen">
+            <Desktop background={'/background.jpg'} />
             <Taskbar />
         </main>
     );
---
+++ src/components/desktop.tsx
@@ -0,0 +1,36 @@
+import { FaAmazon, FaGoogle, FaNode, FaReact, FaTwitch } from "react-icons/fa";
+
+export function Desktop({background}) {
+    return (
+        <div className="fixed -z-10 top-0 left-0 w-full h-full bg-center bg-no-repeat bg-cover" style={{backgroundImage: 'url(' + background + ')'}}>
+            <div id="desktop-icons" className="w-full h-full px-5 py-5 flex-col flex-wrap items-start flex">
+                <DesktopItem text={"Goggle"}>
+                    <FaGoogle style={{height: '32px', width: '32px'}} />
+                </DesktopItem>
+                <DesktopItem text={"Goggle1"}>
+                    <FaAmazon style={{height: '32px', width: '32px'}} />
+                </DesktopItem>
+                <DesktopItem text={"Goggle2"}>
+                    <FaNode style={{height: '32px', width: '32px'}} />
+                </DesktopItem>
+                <DesktopItem text={"Goggle3"}>
+                    <FaReact style={{height: '32px', width: '32px'}} />
+                </DesktopItem>
+                <DesktopItem text={"Goggle4"}>
+                    <FaTwitch style={{height: '32px', width: '32px'}} />
+                </DesktopItem>
+            </div>
+        </div>
+    );
+}
+
+function DesktopItem({children, text="Icon Text"}) {
+    return (
+        <div className="desktop-item w-20 flex flex-col items-center mb-[3px] mt-[3px] pt-[12px] pb-[8px] -outline-offset-3 hover:bg-[#bfbfbf70] hover:outline-[#a2cef4] hover:outline focus:bg-[#b3b3b370] active:bg-[#b3b3b370]">
+            <span className="w-8 h-8 flex items-center justify-center">
+                {children}
+            </span>
+            <span className="text-ellipsis w-full text-center whitespace-pre overflow-hidden select-none">{text}</span>
+        </div>
+    );
+}
