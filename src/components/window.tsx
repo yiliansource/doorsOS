1e21262572586b5ea1d4d80d3c3f88a472313a2a
mellbacon <mellodev@outlook.com>
2025-03-18 15:36:29 -0500

    added active background colors to the window buttons
    Signed-off-by: mellbacon <mellodev@outlook.com>

--- src/components/window.tsx
+++ src/components/window.tsx
@@ -12,9 +12,9 @@ function Header({ title = "Window" }: { title?: string }) {
         <div className="header h-[30px] w-full flex items-center justify-between select-none text-black pl-[6px]">
             <div className="title overflow-hidden whitespace-nowrap text-ellipsis text-[14px]">{title}</div>
             <div className="window-buttons flex gap-[5px] items-center justify-center h-full flex-row">
-                <button className="text-[0.875rem] flex items-center justify-center min-w-[37px] h-full bg-[#ffffff] active:outline-none before:content-['\2014'] hover:bg-[#E9E9E9]"></button>
-                <button className="text-[0.875rem] flex items-center justify-center min-w-[37px] h-full bg-[#ffffff] active:outline-none before:content-['\2610'] hover:bg-[#E9E9E9]"></button>
-                <button className="text-[0.875rem] flex items-center justify-center min-w-[37px] h-full bg-[#ffffff] active:outline-none before:content-['\2715'] hover:bg-red-600"></button>
+                <button className="text-[0.875rem] flex items-center justify-center min-w-[37px] h-full bg-[#ffffff] active:outline-none before:content-['\2014'] hover:bg-[#E9E9E9] active:bg-[#bbb]"></button>
+                <button className="text-[0.875rem] flex items-center justify-center min-w-[37px] h-full bg-[#ffffff] active:outline-none before:content-['\2610'] hover:bg-[#E9E9E9] active:bg-[#bbb]"></button>
+                <button className="text-[0.875rem] flex items-center justify-center min-w-[37px] h-full bg-[#ffffff] active:outline-none before:content-['\2715'] hover:bg-red-600 hover:color-[#fff] active:bg-red-400"></button>
             </div>
         </div>
     );
