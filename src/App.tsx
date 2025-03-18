17c22e8f33b1eba36ddb656882dc2376317e1414
Ian Hornik <yiliansource@gmail.com>
2025-03-18 09:45:57 +0100

    Feature/desktop (#3)
    * added desktop component
    
    note: removed h-screen because it was conflicting with desktop interactivity
    Signed-off-by: mellbacon <mellodev@outlook.com>
    
    * added desktop window
    
    Signed-off-by: mellbacon <mellodev@outlook.com>
    
    * implemented pr review
    
    ---------
    
    Signed-off-by: mellbacon <mellodev@outlook.com>
    Co-authored-by: mellbacon <mellodev@outlook.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import { ErrorBoundary } from "react-error-boundary";
+import { Desktop } from "./components/desktop";
 import { Taskbar } from "./components/taskbar";
 import { BlueScreen } from "./components/blue-screen";
+import { ErrorBoundary } from "react-error-boundary";
 
 function App() {
     return (
         <main className="relative w-screen h-screen">
             <ErrorBoundary FallbackComponent={BlueScreen}>
-                <div className="fixed -z-10 top-0 left-0 w-full h-full">
-                    <img src="/background.jpg" className="w-full h-full" />
-                </div>
-
+                <Desktop />
                 <Taskbar />
             </ErrorBoundary>
         </main>
